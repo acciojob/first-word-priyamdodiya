24bb4fe016ef5f43200b72abf70ddb7270f09b71
priyamdodiya <126169432+priyamdodiya@users.noreply.github.com>
2025-04-11 09:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	let a=s.trim().split(" ");
+	for(let i=0;i<=s.length;i++){
+		let firstword=a[0];
+		return firstword;
+	}
 }
 
 // Do not change the code below
